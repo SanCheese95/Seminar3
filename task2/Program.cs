332e2ceb0b07afe8f3638eb099b500885ea4dc6c
Sancheese95 <sasha_pavlyukovec@mail.ru>
2023-07-16 06:29:39 +0300

    done homework

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double FindDistance(int xA,int yA,int zA,int xB,int yB,int zB)
+{
+    double lenght = Math.Sqrt((xB-xA)*(xB-xA)+(yB-yA)*(yB-yA)+(zB-zA)*(zB-zA));
+    return lenght;
+}
+
+int GetInfo (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int coorxA = GetInfo("Введите координату x числа A: ");
+int cooryA = GetInfo("Введите координату y числа A: ");
+int coorzA = GetInfo("Введите координату z числа A: ");
+int coorxB = GetInfo("Введите координату x числа B: ");
+int cooryB = GetInfo("Введите координату y числа B: ");
+int coorzB = GetInfo("Введите координату z числа B: ");
+
+Console.WriteLine($"A ({coorxA},{cooryA},{coorzA}); B ({coorxB},{cooryB},{coorzB}) -> {FindDistance(coorxA, cooryA, coorzA, coorxB, cooryB, coorzB)}");
---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27 
+// 5 -> 1, 8, 27, 64, 125
+
+void DefCube(int num)
+{
+    Console.Write($"{num} -> ");
+        for(int i = 1; i < num; i++)
+        Console.Write(i * i * i + ", ");
+    Console.Write(num * num * num +".");
+}
+
+int GetInfo (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число: ");
+DefCube(num);
